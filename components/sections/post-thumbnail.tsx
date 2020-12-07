fa724d8fe1d48ec7e862031b24c144cdda4a1732
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2020-12-07 23:55:10 +0530

    fix: change primary font

--- components/sections/post-thumbnail.tsx
+++ components/sections/post-thumbnail.tsx
@@ -9,7 +9,7 @@ const PostThumbnail: React.FC<PostThumbnailProps> = ({ post }) => {
   return (
     <div>
       {post.coverImage && <img src={post.coverImage} className="rounded-lg w-full h-64 object-cover mb-4" alt="" />}
-      <h4 className="text-xl font-medium">{post.title}</h4>
+      <h4 className="text-xl font-medium mb-4">{post.title}</h4>
       <p className="text-gray-600 line-clamped text-sm">{post.brief}</p>
       <a target="_blank" rel="noopener noreferrer" href={`${config.blogHost}/${post.slug}`}>Read more</a>
     </div>
